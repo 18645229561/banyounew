0600cacaa27b344cae7a4db5226803130d5ff8a3
RenPeng <renpeng511226@gmail.com>
2017-05-30 11:05:48 +0800

    tttt

--- app/src/main/java/com/example/renpeng/banyou/LoginActivity.java
+++ app/src/main/java/com/example/renpeng/banyou/LoginActivity.java
@@ -6,19 +6,33 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.text.Html;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.loopj.android.http.TextHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 /**
  * Created by renpeng on 17/5/29.
  */
 public class LoginActivity extends FragmentActivity implements View.OnClickListener{
 
-    TextView mTextView;
+    private TextView mTextView;
+    private  Button mButton;
+    private EditText usernameEdit;
+    private EditText passwordEdit;
 
-    Button mButton;
+    private String username = "";
+    private String password = "";
 
 
     @Override
@@ -27,12 +41,25 @@ public class LoginActivity extends FragmentActivity implements View.OnClickListe
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.login_activity_layout);
 
+        initView();
+
+    }
+
+    private void initView(){
         mTextView = (TextView) findViewById(R.id.register);
         mTextView.setText(Html.fromHtml("<u>"+"没有账号?快来注册!"+"</u>"));
-
         mButton = (Button) findViewById(R.id.login);
+
+        usernameEdit = (EditText) findViewById(R.id.username);
+        passwordEdit = (EditText) findViewById(R.id.password);
+
         mButton.setOnClickListener(this);
         mTextView.setOnClickListener(this);
+    }
+
+    private void getUserLoginInfo(){
+        username = usernameEdit.getText().toString();
+        password = passwordEdit.getText().toString();
     }
 
     public static void startLoginActivity(Activity mActivity)
@@ -46,7 +73,7 @@ public class LoginActivity extends FragmentActivity implements View.OnClickListe
         switch (v.getId())
         {
             case R.id.login:
-                HomeActivity.startHomeActivity(LoginActivity.this);
+                login();
                 break;
             case R.id.register:
                 RegisterActivity.startRegisterActivity(LoginActivity.this);
@@ -54,4 +81,50 @@ public class LoginActivity extends FragmentActivity implements View.OnClickListe
                 break;
         }
     }
+
+    private void login(){
+        getUserLoginInfo();
+
+        if(TextUtils.isEmpty(username))
+        {
+            Toast.makeText(LoginActivity.this,"请输入用户名",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(LoginActivity.this,"请输入密码",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+
+        String host = "http://192.168.1.101:8080/thundertech";
+        String getUrl = host + "/user/login?username="+username+ "&password=" + password;
+        HttpUtils.get(getUrl, new TextHttpResponseHandler() {
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                Toast.makeText(LoginActivity.this,"访问错误",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, String responseString) {
+                String text = responseString.substring(responseString.indexOf("{"));
+                JSONTokener tokener = new JSONTokener(text);
+
+                try {
+                    JSONObject json = (JSONObject) tokener.nextValue();
+                    String result = json.getString("result");
+                    String info = json.getString("info");
+                    if("true" == result){
+                        HomeActivity.startHomeActivity(LoginActivity.this);
+                    }else{
+                        Toast.makeText(LoginActivity.this,info,Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
 }
