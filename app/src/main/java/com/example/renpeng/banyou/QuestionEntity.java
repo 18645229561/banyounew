143a80021a8e469153842e216a13ea96faf79bb9
RenPeng <renpeng511226@gmail.com>
2017-05-30 16:24:01 +0800

    tttt

---
+++ app/src/main/java/com/example/renpeng/banyou/QuestionEntity.java
@@ -0,0 +1,11 @@
+package com.example.renpeng.banyou;
+
+/**
+ * Created by renpeng on 17/5/30.
+ */
+public class QuestionEntity {
+
+    public String content;
+
+    public String id;
+}
--- app/src/main/java/com/example/renpeng/banyou/QuestionInfoActivity.java
+++ app/src/main/java/com/example/renpeng/banyou/QuestionInfoActivity.java
@@ -4,24 +4,37 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.loopj.android.http.TextHttpResponseHandler;
 
 import org.apache.http.Header;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by renpeng on 17/5/30.
  */
 public class QuestionInfoActivity extends FragmentActivity {
 
+    private ListView mListView;
+
+    private QuestionAdapter questionAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.question_activity_layout);
+
+        mListView = (ListView) findViewById(R.id.list);
+
         getQuestionInfo();
     }
 
@@ -30,12 +43,17 @@ public class QuestionInfoActivity extends FragmentActivity {
         mActivity.startActivity(intent);
     }
 
+    private void setData(List<String> list){
+        questionAdapter = new QuestionAdapter(list);
+        mListView.setAdapter(questionAdapter);
+    }
+
     public void getQuestionInfo(){
         String url = "http://192.168.1.101:8080/thundertech/subject/getSubjects";
         HttpUtils.get(url, new TextHttpResponseHandler() {
             @Override
             public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-
+                Toast.makeText(QuestionInfoActivity.this,"出错啦!",Toast.LENGTH_SHORT).show();
             }
 
             @Override
@@ -46,17 +64,25 @@ public class QuestionInfoActivity extends FragmentActivity {
                 try {
                     JSONObject json = (JSONObject) tokener.nextValue();
                     String result = json.getString("result");
-                    String info = json.getString("info");
-                    if("true" == result){
-                        Log.d("info",info);
-                    }else{
+                    JSONArray jsonArray = json.getJSONArray("info");
+                    int n = jsonArray.length();
+                    List<String> list = new ArrayList<String>();
+                    for(int i = 0;i<n ; i++)
+                    {
+                        String question = ((JSONObject)(jsonArray.get(i))).getString("content");
+                        list.add(question);
+                    }
 
+                    if("true" == result){
+                        setData(list);
+                       // Toast.makeText(QuestionInfoActivity.this,"---"+list.size(),Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(QuestionInfoActivity.this,"出错啦!",Toast.LENGTH_SHORT).show();
                     }
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
             }
         });
-
     }
 }
