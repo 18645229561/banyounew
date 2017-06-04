9d197c6390e3bb0117e549665bcdf3ec0d491224
RenPeng <renpeng511226@gmail.com>
2017-06-04 14:31:05 +0800

    test

---
+++ app/src/main/java/com/example/renpeng/banyou/TestDetailActivity.java
@@ -0,0 +1,33 @@
+package com.example.renpeng.banyou;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.webkit.WebView;
+
+/**
+ * Created by renpeng on 17/6/4.
+ */
+public class TestDetailActivity extends FragmentActivity {
+    private WebView webView;
+
+    private String type;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scenery_detail_activity);
+
+        webView = (WebView) findViewById(R.id.scenery_detail);
+        webView.getSettings().setJavaScriptEnabled(true);
+        type = getIntent().getStringExtra("type");
+        webView.loadUrl("http://47.93.205.29/thundertech/subject/" + type);
+    }
+
+    public static void startTestDetailActivity(Activity activity, String type){
+        Intent intent = new Intent(activity,SceneryDetailActivity.class);
+        intent.putExtra("type",type);
+        activity.startActivity(intent);
+    }
+}
--- app/src/main/java/com/example/renpeng/banyou/TestFragment.java
+++ app/src/main/java/com/example/renpeng/banyou/TestFragment.java
@@ -5,11 +5,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by renpeng on 17/5/31.
  */
-public class TestFragment extends Fragment{
+public class TestFragment extends Fragment implements View.OnClickListener{
+
+    private TextView medium;
+    private TextView high;
+
+    private String type;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -17,6 +23,25 @@ public class TestFragment extends Fragment{
     {
         View view = inflater.inflate(R.layout.view_page_3, container, false);
 
+        medium = (TextView) view.findViewById(R.id.test1);
+        high = (TextView) view.findViewById(R.id.test2);
+
+        medium.setOnClickListener(this);
+        high.setOnClickListener(this);
+
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.test1:
+                type = "TestJunior";
+            case R.id.test2:
+                type = "TestSenior";
+                break;
+        }
+        TestDetailActivity.startTestDetailActivity(getActivity(),type);
+
     }
 }
